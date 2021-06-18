26f3f7def3dbf1aaab322d80b3fa652b641d4e86
RyanRennison <rmrennison@gmail.com>
2021-06-18 10:14:04 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+var images = document.querySelectorAll(".block img");
+
+images.forEach(function (image) {
+    image.addEventListener("mouseover", function () {
+        this.classList.add('active');
+    });
+    image.addEventListener("mouseout", function () {
+        this.classList.remove('active');
+    });
+});
